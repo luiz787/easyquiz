6181cca614e2c2da00db0f613d7037d69617359b
Victor César de Paula Rangel <playcontasvictor2@gmail.com>
2017-05-23 12:17:14 -0300

    Adiciona a classe ManterPostImplTest(incompleta)

---
+++ QuizAppBusiness/test/model/service/ManterPostImplTest.java
@@ -0,0 +1,16 @@
+package model.service;
+
+import java.util.List;
+import model.dao.PostDAOImpl;
+import model.domain.Post;
+
+
+public class ManterPostImplTest {
+    private static ManterPost manterPost;
+    private static PostDAOImpl postDAOImpl;
+    private Post post;
+    private static List<Post> comentarios;
+
+    public ManterPostImplTest() {}
+    
+}
