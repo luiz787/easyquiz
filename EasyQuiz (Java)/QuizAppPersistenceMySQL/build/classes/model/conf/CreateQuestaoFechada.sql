96e5771277aaa60c9eeed6390c8ae62856634fa1
Luiz <diasbertoluiz@gmail.com>
2017-07-06 09:05:05 -0300

    Remocao de projetos duplicados

--- EasyQuiz (Java)/QuizAppPersistenceMySQL/build/classes/model/conf/CreateQuestaoFechada.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS questao_fechada CASCADE;
-
-CREATE TABLE `questao_fechada` (
-  `Id` int(11) NOT NULL PRIMARY KEY ,
-  `Alternativas` text NOT NULL,
-  `Alternativa_Correta` int(1) NOT NULL
-)
-
