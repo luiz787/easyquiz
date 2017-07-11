5a2e12d001fbd1e255e5cdb92ce907e3be16ad48
Victor César de Paula Rangel <playcontasvictor2@gmail.com>
2017-07-11 03:34:44 -0300

    Add escolaridade ao DER e melhora o controlador de cadastro

--- EasyQuiz (Java)/EasyQuiz/src/java/controller/Cadastra.java
+++ EasyQuiz (Java)/EasyQuiz/src/java/controller/Cadastra.java
@@ -4,7 +4,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.sql.Date;
+import java.text.DateFormat;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
@@ -12,22 +13,44 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.dao.UsuarioDAO;
+import model.daoimpl.UsuarioDAOImpl;
+import model.domain.Perfil;
+import model.domain.Usuario;
+import model.serviceimpl.ManterUsuarioImpl;
 
 @WebServlet(name = "Cadastra", urlPatterns = {"/Cadastra"})
 public class Cadastra extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        
+        Perfil perfil;
+        Usuario usuario;
+        ManterUsuarioImpl manterUsuario;
+        UsuarioDAO usuarioDAO;
+            
+        try {
             String nome = request.getParameter("nome");
             String email = request.getParameter("email");
-            SimpleDateFormat formato = new SimpleDateFormat("dd-MM-yyyy");
-            try {
-                Date data = formato.parse(request.getParameter("data"));
-            } catch (ParseException ex) {
-                Logger.getLogger(Cadastra.class.getName()).log(Level.SEVERE, null, ex);
-            }
+            String strdata = request.getParameter("data");
+            DateFormat formato = new SimpleDateFormat("dd-MM-yyyy");
+            Date data = new Date(formato.parse(strdata).getTime());
             String escolaridade = request.getParameter("escolaridade");
             String senha = request.getParameter("senha");
-            String confirma_senha = request.getParameter("confirma_senha");
+            
+            perfil = new Perfil();
+            perfil.setNome(nome);
+            
+            usuario = new Usuario();
+            usuario.setPerfil(perfil);
+            usuario.setEmail(email);
+            usuario.setDataNascimento(data);
+            usuario.setEscolaridade(escolaridade);
+            usuario.setSenha(senha);
+        } catch (ParseException ex) {
+            Logger.getLogger(Cadastra.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
