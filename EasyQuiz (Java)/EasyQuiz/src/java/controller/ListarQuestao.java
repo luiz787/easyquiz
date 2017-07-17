3e94affa0f4e4512650eb0f3da9c11a64d813e70
RafaelGSNeves <rafaelneves.99@hotmail.com>
2017-07-16 23:15:01 -0300

    atualização busca
    insere funcionalidade da busca

--- EasyQuiz (Java)/EasyQuiz/src/java/controller/ListarQuestao.java
+++ EasyQuiz (Java)/EasyQuiz/src/java/controller/ListarQuestao.java
@@ -23,10 +23,66 @@ public class ListarQuestao {
             
             ManterQuestao manterQuestao = new ManterQuestaoImpl(QuestaoDAOImpl.getInstance());
             List<Questao> listQuestao = manterQuestao.getAll();
-
             ManterQuestaoFechada manterQuestaoFechada = 
                      new ManterQuestaoFechadaImpl(QuestaoFechadaDAOImpl.getInstance());
             List<QuestaoFechada> listQuestaoFechada = manterQuestaoFechada.getAll();
+            
+          
+             
+            for (int i=0; i < listQuestao.size(); i++ ) { 
+               Questao Q = listQuestao.get(i);
+           if ( request.getParameter("nivel")!=  null  )  {
+               if(!request.getParameter("nivel").equals(""))  { 
+                 if(! Q.getDificuldade().getId().toString().equals(request.getParameter("nivel"))){
+                        listQuestao.remove(Q);
+                        i--;
+                        continue;
+                    }
+               }
+           }
+           if ( request.getParameter("materia") !=  null ) {
+               if(!request.getParameter("materia").equals(""))  { 
+                        if(! Q.getDisciplina().getId().toString().equals(request.getParameter("materia"))){
+                        listQuestao.remove(Q);
+                        i--;
+                        continue;
+                    }
+               }        
+            }
+             if ( request.getParameter("modulo") !=  null ) {
+                 if(!request.getParameter("nivel").equals(""))  { 
+                        if(! Q.getModulo().getId().toString().equals(request.getParameter("modulo"))){
+                        listQuestao.remove(Q);
+                        i--;
+                        continue;
+                    }
+                 }
+            }
+            if ( request.getParameter("tipo") !=  null ) {
+                if(!request.getParameter("tipo").equals(""))  { 
+                        System.out.println("tipo request:" +request.getParameter("tipo") + " tipo questao: "+ Q.getIdTipo() );
+                        if(!request.getParameter("tipo").contains(String.valueOf(Q.getIdTipo())) ){
+                    
+                            listQuestao.remove(Q);
+                            i--;
+                            continue;
+                    }
+                }        
+            }
+            if ( request.getParameter("palavras") !=  null ) {
+                if(!request.getParameter("palavras").equals(""))  { 
+                        if(! Q.getTxtEnunciado().contains(request.getParameter("palavras"))){
+                        listQuestao.remove(Q);
+                        i--;
+                        continue;
+                    }
+                }        
+            }
+            
+             
+            }
+            
+            
             
             if(request.getSession().getAttribute("cod_Usuario")!=null) {
                 ListarQuestaoFechadaResposta.execute(request);
